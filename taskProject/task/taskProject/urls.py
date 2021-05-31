15afd15cb47469d4f295579bca3b2fc3d5e0db74
EzequielCosta <ezequielteixeiracosta321@gmail.com>
2021-05-31 13:55:30 -0300

    Mudança da url

--- taskProject/task/taskProject/urls.py
+++ taskProject/task/taskProject/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('quadro/editar/<int:id>/', views.editar_quadro, name='editar_quadro'),
     path('quadro/excluir/<int:id>/', views.excluir_quadro, name='excluir_quadro'),
     path('categoria/cadastrar/', views.cadastro_categoria, name='cadastro_categoria'),
-    path('categoria/exibir/', views.exibir_categoria, name='exibir_categoria'),
+    path('categoria/', views.exibir_categoria, name='exibir_categoria'),
     path('categoria/editar/<int:id>/', views.editar_categoria, name='editar_categoria'),
     path('categoria/excluir/<int:id>/', views.excluir_categoria, name='excluir_categoria'),
 ]
